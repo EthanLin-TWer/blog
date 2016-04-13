378b1e62d414195c011fc6e6d1439c42f7bb00fe
linesh-simplicity <linesh.simpcity@gmail.com>
2016-04-13 19:37:56 +0800

    Linesh: Use ng-repeat and github api to fetch posts data

--- app/posts/posts-controller.js
+++ app/posts/posts-controller.js
@@ -1,5 +1,8 @@
 'use strict';
 
-export default ['$scope', ($scope) => {
-    $scope.hello = 'hello world';
+export default ['$scope', '$http', ($scope, $http) => {
+    $scope.posts = $http.get('https://api.github.com/repos/linesh-simplicity/linesh-simplicity.github.io/contents/_posts/')
+        .then(function(response) {
+            $scope.posts = response.data;
+        });
 }];
