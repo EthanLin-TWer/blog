7a0d67f73256a2278405b4dc16a6734721926d73
linesh-simplicity <linesh.simpcity@gmail.com>
2016-04-14 11:39:48 +0800

    Linesh: Use github raw content api since Github API has GET counts limit

--- app/posts/posts-controller.js
+++ app/posts/posts-controller.js
@@ -1,8 +1,15 @@
 'use strict';
 
 export default ['$scope', '$http', ($scope, $http) => {
-    $scope.posts = $http.get('https://api.github.com/repos/linesh-simplicity/linesh-simplicity.github.io/contents/_posts/')
+    $scope.posts = [];
+
+    $http.get('https://raw.githubusercontent.com/linesh-simplicity/linesh-simplicity.github.io/master/_posts/2016-03-11-elegant-mac-iterm2.md')
         .then(function(response) {
-            $scope.posts = response.data;
-        });
+            $scope.posts.push(response.data);
+    });
+
+    $http.get('https://raw.githubusercontent.com/linesh-simplicity/linesh-simplicity.github.io/master/_posts/2016-03-13-elegant-mac-oh-my-zsh.md')
+        .then(function(response) {
+            $scope.posts.push(response.data);
+    });
 }];
