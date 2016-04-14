710c08d5b8dc1fc65180f7d8c1569f06f3b6eb00
linesh-simplicity <linesh.simpcity@gmail.com>
2016-04-14 16:20:02 +0800

    Linesh: resolve #29, using $q to encapsulate http requests

--- app/posts/posts-service.js
+++ app/posts/posts-service.js
@@ -1,15 +1,17 @@
-export default ['$http', function($http) {
+export default ['$http', '$q', function($http, $q) {
     var posts = [];
+    var postsUrl = [
+        'https://raw.githubusercontent.com/linesh-simplicity/linesh-simplicity.github.io/master/_posts/2016-03-11-elegant-mac-iterm2.md',
+        'https://raw.githubusercontent.com/linesh-simplicity/linesh-simplicity.github.io/master/_posts/2016-03-13-elegant-mac-oh-my-zsh.md'
+    ]
 
-    $http.get('https://raw.githubusercontent.com/linesh-simplicity/linesh-simplicity.github.io/master/_posts/2016-03-11-elegant-mac-iterm2.md')
-        .then(response => {
-            posts.push(response.data);
-        });
-
-    $http.get('https://raw.githubusercontent.com/linesh-simplicity/linesh-simplicity.github.io/master/_posts/2016-03-13-elegant-mac-oh-my-zsh.md')
-        .then(response => {
-            posts.push(response.data);
-        });
+    $q.all([
+        $http.get(postsUrl[0]), $http.get(postsUrl[1])
+    ]).then(response=> {
+        for (let item of response) {
+            posts.push(item.data);
+        }
+    });
 
     return {
         getPosts: function() {
