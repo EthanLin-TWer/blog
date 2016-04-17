c991b896f7b4c077cd3d0e020f651296a56813e3
linesh-simplicity <linesh.simpcity@gmail.com>
2016-04-17 20:14:00 +0800

    [#28] Comment used later code, add renju homepage and extract function

--- app/posts/posts-controller.js
+++ app/posts/posts-controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
 export default ['$scope', 'postsService', ($scope, postsService) => {
-    $scope.posts = postsService.getPosts();
+    // $scope.posts = postsService.getPosts();
     $scope.postsMeta = postsService.getDescriptiveMetaInfo();
 }];
--- app/posts/posts-service.js
+++ app/posts/posts-service.js
@@ -1,19 +1,19 @@
 import moment from 'moment'
 
 export default ['$http', '$q', function($http, $q) {
-    var posts = [];
-    var postsUrl = [
-        'https://raw.githubusercontent.com/linesh-simplicity/linesh-simplicity.github.io/master/_posts/2016-03-11-elegant-mac-iterm2.md',
-        'https://raw.githubusercontent.com/linesh-simplicity/linesh-simplicity.github.io/master/_posts/2016-03-13-elegant-mac-oh-my-zsh.md'
-    ];
-
-    $q.all([
-        $http.get(postsUrl[0]), $http.get(postsUrl[1])
-    ]).then(response=> {
-        for (let item of response) {
-            posts.push(item.data);
-        }
-    });
+    // var posts = [];
+    // var postsUrl = [
+    //     'https://raw.githubusercontent.com/linesh-simplicity/linesh-simplicity.github.io/master/_posts/2016-03-11-elegant-mac-iterm2.md',
+    //     'https://raw.githubusercontent.com/linesh-simplicity/linesh-simplicity.github.io/master/_posts/2016-03-13-elegant-mac-oh-my-zsh.md'
+    // ];
+    //
+    // $q.all([
+    //     $http.get(postsUrl[0]), $http.get(postsUrl[1])
+    // ]).then(response=> {
+    //     for (let item of response) {
+    //         posts.push(item.data);
+    //     }
+    // });
 
     var posts_metas = [];
     var postsBaseDirectory = 'https://api.github.com/repos/linesh-simplicity/linesh-simplicity.github.io/contents/_posts/';
@@ -21,12 +21,16 @@ export default ['$http', '$q', function($http, $q) {
     $http.get(postsBaseDirectory).then(function (response){
         for (let item of response.data) {
             var post = {};
-            post.title = item.name;
+            post.title = resolveTitle(item.name);
             post.url = resolveUrl(item.name);
             post.date = resolveDate(item.name);
 
             console.log(post);
             posts_metas.push(post);
+        }
+
+        function resolveTitle(article_name) {
+            return article_name;
         }
 
         function resolveUrl(article_name) {
@@ -45,9 +49,9 @@ export default ['$http', '$q', function($http, $q) {
     });
 
     return {
-        getPosts: function() {
-            return posts;
-        },
+        // getPosts: function() {
+        //     return posts;
+        // },
         getDescriptiveMetaInfo: function() {
             return posts_metas;
         }
