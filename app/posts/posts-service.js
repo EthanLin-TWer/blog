70d30502d6980828621a92af487dd420d8470b5b
linesh-simplicity <linesh.simpcity@gmail.com>
2016-04-21 18:15:43 +0800

    [#40] Fetch post contents from file using angular $http

--- app/posts/posts-service.js
+++ app/posts/posts-service.js
@@ -2,10 +2,16 @@ import moment from 'moment'
 
 export default ['$http', function($http) {
     var posts_metas = [];
+    var posts_contents = {};
 
     $http.get('_posts/').then(
         (response) => {
             for (let post_title of response.data) {
+                populatePostsMetaInformation(post_title);
+                fetchAndPopulatePostsContents(post_title);
+            }
+
+            function populatePostsMetaInformation(post_title) {
                 var post = {};
 
                 post.title = post_title;
@@ -18,6 +24,17 @@ export default ['$http', function($http) {
             function extractDate(post_title) {
                 // article_name is in '2016-03-28-article-name.md' like format
                 return post_title.substring(0, 10);
+            }
+
+            function fetchAndPopulatePostsContents(post_title) {
+                var post_contents = {};
+
+                posts_contents.title = post_title;
+                $http.get('_posts/' + post_title).then(
+                    (post_contents) => {
+                        posts_contents.contents = post_contents.data;
+                    }
+                )
             }
         });
 
