d2c4abbb9a118735b6c6ec7c05e0b5749323313e
linesh-simplicity <linesh.simpcity@gmail.com>
2016-04-21 19:19:55 +0800

    [#28] Code refactor

--- app/posts/posts-service.js
+++ app/posts/posts-service.js
@@ -2,43 +2,35 @@ import moment from 'moment'
 
 export default ['$http', function($http) {
     var posts_metas = [];
-    var posts_contents = {};
 
     $http.get('_posts/').then(
         (response) => {
-            for (let post_title of response.data) {
-                populatePostsMetaInformation(post_title);
-                fetchAndPopulatePostsContents(post_title);
-            }
+            for (let file_name of response.data) {
+                $http.get('_posts/' + file_name).then(
+                    response => {
+                        var post_details = {};
 
-            function populatePostsMetaInformation(post_title) {
-                var post = {};
+                        post_details.title = file_name;
+                        post_details.date = moment(extractDate(file_name)).format('DD-MMM-GGGG');
+                        post_details.contents = response.data;
+                        post_details.url = '#posts/' + extractUrl(file_name);
 
-                post.title = post_title;
-                post.url = '#posts/' + extractTitle(post_title);
-                post.date = moment(extractDate(post_title)).format('DD-MMM-GGGG');
-
-                posts_metas.push(post);
-            }
-
-            function extractTitle(post_title) {
-                return post_title.substring(0, post_title.indexOf('.md'));
-            }
-
-            function extractDate(post_title) {
-                // article_name is in '2016-03-28-article-name.md' like format
-                return post_title.substring(0, 10);
-            }
-
-            function fetchAndPopulatePostsContents(post_title) {
-                var post_contents = {};
-
-                posts_contents.title = post_title;
-                $http.get('_posts/' + post_title).then(
-                    (post_contents) => {
-                        posts_contents.contents = post_contents.data;
+                        posts_metas.push(post_details);
                     }
-                )
+                );
+
+                function extractTitle(file_name) {
+                    return file_name;
+                }
+
+                function extractUrl(file_name) {
+                    return file_name.substring(0, file_name.indexOf('.md'));
+                }
+
+                function extractDate(file_name) {
+                    // article_name is in '2016-03-28-article-name.md' like format
+                    return file_name.substring(0, 10);
+                }
             }
         });
 
