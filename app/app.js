074178181ada0c2b718a507fd6c7fa97d589d710
linesh-simplicity <linesh.simpcity@gmail.com>
2016-04-21 21:25:08 +0800

    [#40] Using angular-marked/highlightjs to apply style to post page

--- app/app.js
+++ app/app.js
@@ -7,6 +7,8 @@ import aboutController from './about/about-controller'
 
 import postsService from './posts/posts-service'
 
+import hljs from 'highlight.js'
+
 angular.module('BlogApp', [
         ngRoute,
         hcMarked
@@ -18,11 +20,29 @@ angular.module('BlogApp', [
     .config(function($routeProvider) {
         $routeProvider
             .when('/', {
-                templateUrl: '/app/posts/posts.html',
+                templateUrl: '/app/posts/home.html',
+                controller: 'postsController'
+            })
+            .when('/posts/:postId', {
+                templateUrl: '/app/posts/post.html',
                 controller: 'postsController'
             })
             .when('/about', {
                 templateUrl: '/app/about/about.html',
                 controller: 'aboutController'
             });
-    });
+    })
+    .config(['markedProvider', function (markedProvider) {
+        markedProvider.setOptions({
+            gfm: true,
+            tables: true,
+            highlight: function (code, lang) {
+                if (lang) {
+                    return hljs.highlight(lang, code, true).value;
+                } else {
+                    return hljs.highlightAuto(code).value;
+                }
+            }
+        });
+    }])
+    ;
