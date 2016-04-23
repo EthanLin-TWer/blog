79328ab21631bb69b8a745c999fb2d60a46754a8
linesh-simplicity <linesh.simpcity@gmail.com>
2016-04-24 01:41:34 +0800

    [#45] Refactor, use front-matter for better naming instead of 'header'

--- precommit.js
+++ precommit.js
@@ -5,11 +5,11 @@ var posts = fs.readdirSync('./_posts').filter(item => item.endsWith('.md'));
 */
 var posts_meta = posts.map(item => {
     var contents = fs.readFileSync('./_posts/' + item, 'utf-8');
-    var jekyll_header = contents.split('---')[1];
+    var jekyll_front_matter = contents.split('---')[1];
 
     return {
         'key': item.substring(0, item.lastIndexOf('.')),
-        'title': jekyll_header.substring((jekyll_header.indexOf('title: ') + 'title: '.length), jekyll_header.length).trim()
+        'title': jekyll_front_matter.substring((jekyll_front_matter.indexOf('title: ') + 'title: '.length), jekyll_front_matter.length).trim()
     }
 });
 
@@ -17,11 +17,11 @@ fs.writeFileSync('posts-meta.json', JSON.stringify(posts_meta).trim(), 'utf-8');
 
 var posts_content = posts.map(item => {
     var contents = fs.readFileSync('./_posts/' + item, 'utf-8');
-    var jekyll_header = contents.split('---')[1];
+    var jekyll_front_matter = contents.split('---')[1];
 
     return {
         'key': item.substring(0, item.lastIndexOf('.')),
-        'title': jekyll_header.substring((jekyll_header.indexOf('title: ') + 'title: '.length), jekyll_header.length).trim(),
+        'title': jekyll_front_matter.substring((jekyll_front_matter.indexOf('title: ') + 'title: '.length), jekyll_front_matter.length).trim(),
         'contents': contents.split('---')[2]
     }
 });
