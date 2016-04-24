9d3e89baac9b78180c3a2cae06b3e62b937bbc61
linesh-simplicity <linesh.simpcity@gmail.com>
2016-04-24 12:27:59 +0800

    [#45] Refactor, give posts info preparation function a better name

--- hooks/precommit.js
+++ hooks/precommit.js
@@ -1,18 +1,5 @@
 fs = require('fs');
-
-// Fetch all posts and populate post info for posts_meta and posts_content separate use
-var posts_info = fs.readdirSync('./_posts').filter(item => item.endsWith('.md'))
-    .map(post_path => {
-        var contents = fs.readFileSync('./_posts/' + post_path, 'utf-8');
-        var jekyll_front_matter = contents.split('---')[1];
-
-        return {
-            'key': post_path.substring(0, post_path.lastIndexOf('.')),
-            'name': post_path,
-            'title': jekyll_front_matter.substring((jekyll_front_matter.indexOf('title: ') + 'title: '.length), jekyll_front_matter.length).trim(),
-            'contents': contents.split('---')[2]
-        }
-    });
+var posts_info = fetchPostsContentFromPostDirectoryAndPreparePostsInfoObject();
 
 var posts_meta = posts_info.map(info => {
     return {
@@ -21,7 +8,6 @@ var posts_meta = posts_info.map(info => {
         'title': info.title
     }
 })
-
 fs.writeFileSync('posts-meta.json', JSON.stringify(posts_meta).trim(), 'utf-8');
 
 var posts_content = posts_info.map(info => {
@@ -31,5 +17,19 @@ var posts_content = posts_info.map(info => {
         'contents': info.contents
     }
 })
-
 fs.writeFileSync('posts-content.json', JSON.stringify(posts_content).trim(), 'utf-8');
+
+function fetchPostsContentFromPostDirectoryAndPreparePostsInfoObject() {
+    return fs.readdirSync('./_posts').filter(item => item.endsWith('.md'))
+        .map(post_path => {
+            var contents = fs.readFileSync('./_posts/' + post_path, 'utf-8');
+            var jekyll_front_matter = contents.split('---')[1];
+
+            return {
+                'key': post_path.substring(0, post_path.lastIndexOf('.')),
+                'name': post_path,
+                'title': jekyll_front_matter.substring((jekyll_front_matter.indexOf('title: ') + 'title: '.length), jekyll_front_matter.length).trim(),
+                'contents': contents.split('---')[2]
+            }
+    });
+}
