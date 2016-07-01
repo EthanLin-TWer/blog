cf46536b777f505f3e7d5e26156c75d97a40d0d5
linesh <linesh.simpcity@gmail.com>
2016-07-01 23:36:18 +0800

    [#0] Finish final tweak of the post and open code highlight feature

--- app/app.js
+++ app/app.js
@@ -38,17 +38,17 @@ angular.module('BlogApp', [
     .config(CacheFactoryProvider => {
         angular.extend(CacheFactoryProvider.defaults, { maxAge: 15 * 60 * 1000 });
     })
-    // .config(['markedProvider', function (markedProvider) {
-    //     markedProvider.setOptions({
-    //         gfm: true,
-    //         tables: true,
-    //         highlight: function (code, lang) {
-    //             if (lang) {
-    //                 return hljs.highlight(lang, code, true).value;
-    //             } else {
-    //                 return hljs.highlightAuto(code).value;
-    //             }
-    //         }
-    //     });
-    // }])
+    .config(['markedProvider', function (markedProvider) {
+        markedProvider.setOptions({
+            gfm: true,
+            tables: true,
+            highlight: function (code, lang) {
+                if (lang) {
+                    return hljs.highlight(lang, code, true).value;
+                } else {
+                    return hljs.highlightAuto(code).value;
+                }
+            }
+        });
+    }])
     ;
