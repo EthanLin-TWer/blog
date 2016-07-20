55d7e599e61e0967e1733fa82ce37d6f327efe82
linesh <linesh.simpcity@gmail.com>
2016-07-20 19:06:08 +0800

    [#62] Bug fix, get content using substring function so that strings containing '---' will not be excluded

--- hooks/precommit-generate-post-json.js
+++ hooks/precommit-generate-post-json.js
@@ -30,13 +30,14 @@ function fetchPostsContentFromPostDirectoryAndPreparePostsInfoObject() {
     return fs.readdirSync('./_posts').filter(item => item.endsWith('.md'))
         .map(post_path => {
             var contents = fs.readFileSync('./_posts/' + post_path, 'utf-8');
+            const content_start_index = contents.indexOf(contents.split('---')[2]);
             var jekyll_front_matter = contents.split('---')[1];
 
             return {
                 'id': post_path.substring(0, post_path.lastIndexOf('.')),
                 'name': post_path,
                 'title': jekyll_front_matter.substring((jekyll_front_matter.indexOf('title: ') + 'title: '.length), jekyll_front_matter.length).trim(),
-                'contents': contents.split('---')[2]
+                'contents': contents.substring(content_start_index, contents.length)
             }
     });
 }
