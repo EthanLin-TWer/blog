1095ff92c7c1ffa3737c8ee0e8482c5dc23cf8b1
Linesh <linesh.simpcity@gmail.com>
2018-06-24 18:12:01 +0800

    [#208] 🔧chore: add a basic React component without verifying

---
+++ src/react/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Index = () => <div>hello webpack 4 and babel 7</div>
+
+ReactDOM.render(<Index />, document.getElementById('blog'))
