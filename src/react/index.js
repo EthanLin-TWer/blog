981c997afbf2bcf0de37be4fbc47816ecba8e4e0
Linesh <linesh.simpcity@gmail.com>
2018-06-24 19:31:31 +0800

    [#208] 🔧chore: upgrade to @babel 7 beta.41 to support Fragment syntax

--- src/react/index.js
+++ src/react/index.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Index = () => <div>hello webpack 4 and babel 7</div>
+const Index = () => (
+  <>
+    <p> hello webpack 4 </p>
+    <p> and babel 7</p>
+  </>
+)
 
 ReactDOM.render(<Index />, document.getElementById('blog'))
