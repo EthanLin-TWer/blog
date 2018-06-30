6a6d66616f4d40063850a73e497f7411323faea4
Linesh <linesh.simpcity@gmail.com>
2018-07-01 00:09:44 +0800

    [#213] ✨feature: implement a dummy reducer

--- src/react/pages/list/model.js
+++ src/react/pages/list/model.js
@@ -1,8 +1,30 @@
 import Features from '../features'
+import * as service from './service'
 
 export default {
   namespace: Features.BLOG_LIST,
   state: {
-    data: 'different',
+    data: '',
+  },
+  reducers: {
+    saveBlogList(
+      state,
+      {
+        payload: { data },
+      }
+    ) {
+      return { ...state, data: data }
+    },
+  },
+  effects: {
+    *fetchBlogList(action, { call, put }) {
+      const { data } = yield call(service.fetchBlogList)
+      yield put({ type: 'saveBlogList', payload: { data } })
+    },
+  },
+  subscriptions: {
+    setup({ dispatch }) {
+      dispatch({ type: 'fetchBlogList' })
+    },
   },
 }
---
+++ src/react/pages/list/service.js
@@ -0,0 +1,5 @@
+export const fetchBlogList = () => {
+  return {
+    data: 'difference',
+  }
+}
