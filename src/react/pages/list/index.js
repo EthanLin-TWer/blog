36111029f464f8f1e351185611426b48ae341079
Linesh <linesh.simpcity@gmail.com>
2018-08-13 12:26:21 +0800

    [#213] 🔧chore: update api data structure

--- src/react/pages/list/index.js
+++ src/react/pages/list/index.js
@@ -2,14 +2,16 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'dva'
 
+import Features from '../features'
+
 const mapStateToProps = (store) => ({
-  blogs: store.list.data,
+  blogs: store[Features.BLOG_LIST].data,
 })
 
 const BlogList = ({ blogs = [] }) => (
   <div>
     <p>welcome to my blog</p>
-    {blogs.map((blog) => <p key={blog.path}>{blog.name}</p>)}
+    {blogs.map((blog) => <p key={blog.path}>{blog.title}</p>)}
   </div>
 )
 
--- src/react/pages/list/service.js
+++ src/react/pages/list/service.js
@@ -1,7 +1,5 @@
 import axios from '../../utils/axios'
 
 export const fetchBlogList = async () => {
-  return await axios.get(
-    'https://api.github.com/repos/linesh-simplicity/linesh-simplicity.github.io/contents/_posts'
-  )
+  return await axios.get('http://blog.linesh.tw/api/posts.json')
 }
