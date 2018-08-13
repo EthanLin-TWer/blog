011ed7b976a3c24c0180cc455e26af93739eab46
Linesh <linesh.simpcity@gmail.com>
2018-08-13 20:26:59 +0800

    [#213] 🔧chore: should also commit CNAME to be deployed to gh-pages

--- scripts/deploy-react.sh
+++ scripts/deploy-react.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 
+# clean-up
+rm -rf dist
+rm -rf src/react/dist
+mkdir -p dist
+
+# build dist
 npm run build:react
-mv src/react/dist dist/
+
+# copy all necessary files to /dist for deployment to gh-pages branch
+mv src/react/dist/** dist/
+cp CNAME dist/CNAME
