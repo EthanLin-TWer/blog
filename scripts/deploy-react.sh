71fbcc96e53a23b3736c67d5a9ce61136c42f15c
Linesh <linesh.simpcity@gmail.com>
2018-08-20 17:38:05 +0800

    [#5] 🔧chore: introduce webpack and get dev & release mode to work

--- scripts/deploy-react.sh
+++ scripts/deploy-react.sh
@@ -2,14 +2,12 @@
 
 # clean-up
 rm -rf dist
-rm -rf src/react/dist
 mkdir -p dist
 
 # build dist
 npm run build:react
 
 # copy all necessary files to /dist for deployment to gh-pages branch
-mv src/react/dist/** dist/
 cp -r _drafts dist/
 cp -r _posts dist/
 cp -r api dist/
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
+const htmlWebpackPlugin = new HtmlWebPackPlugin({
+  template: 'src/react/index.html',
+  filename: 'index.html',
+})
+
+module.exports = {
+  entry: './src/react/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [htmlWebpackPlugin],
+  devServer: {
+    contentBase: path.join(__dirname, 'src/react'),
+  },
+}
