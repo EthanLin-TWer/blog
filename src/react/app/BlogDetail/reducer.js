776924ec5b2aebc355721d07dabb4f2dfb3c3f66
Linesh <linesh.simpcity@gmail.com>
2018-08-21 23:19:36 +0800

    [#8] 🐛fix: should use Immutable in production

--- src/react/app/BlogDetail/reducer.js
+++ src/react/app/BlogDetail/reducer.js
@@ -1,10 +1,13 @@
+import Immutable from 'seamless-immutable'
+
 import { createReducer } from '../../utils/redux'
 
 import { actions } from './actions'
 
-const initialState = {
+const initialState = Immutable.from({
   posts: {},
-}
+})
+
 export const reducer = createReducer((on) => {
   on(actions.saveBlogDetail, (state, action) => {
     return state.setIn(['posts', action.payload.id], action.payload.data)
