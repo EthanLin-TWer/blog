1b415f4717805509a25364e7827e728ca886a797
Linesh <linesh.simpcity@gmail.com>
2018-08-21 16:31:36 +0800

    [#8] 🎨style: small styling to detail page

--- src/react/components/GithubFlavoredMarkdown/index.js
+++ src/react/components/GithubFlavoredMarkdown/index.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types'
 import ReactMarkdown from 'react-markdown'
 import 'github-markdown-css'
 
+import './styles.styl'
+
 export class GithubFlavoredMarkdown extends React.Component {
   static propTypes = {
     data: PropTypes.string.isRequired,
