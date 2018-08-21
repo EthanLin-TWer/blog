9b9bfe7301eff6d76e48f9102e420af4a3fd4547
Linesh <linesh.simpcity@gmail.com>
2018-08-21 12:29:34 +0800

    [#7] ✨feature: add default not found page

---
+++ src/react/app/NotFound.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export class NotFound extends React.Component {
+  render() {
+    return <div>not found</div>
+  }
+}
--- src/react/app/index.js
+++ src/react/app/index.js
@@ -3,11 +3,13 @@ import '@babel/polyfill'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import BlogList from './BlogList'
+import { NotFound } from './NotFound'
 
 const App = () => (
   <BrowserRouter>
     <Switch>
-      <Route path="/" component={BlogList} />
+      <Route exact path="/" component={BlogList} />
+      <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
 )
