c58adb4a059def8c8dde1cb6d7c2a065df691297
Linesh <linesh.simpcity@gmail.com>
2018-08-21 13:27:51 +0800

    Revert "[#7] ✨feature: add default not found page"
    This will make visiting a post detail served by jekyll by default in Github Pages. Add NotFound page back when detail page is implemented.
    
    This reverts commit 9b9bfe7301eff6d76e48f9102e420af4a3fd4547.
    
    # Conflicts:
    #	src/react/app/index.js

--- src/react/app/NotFound.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export class NotFound extends React.Component {
-  render() {
-    return <div>not found</div>
-  }
-}
--- src/react/app/index.js
+++ src/react/app/index.js
@@ -3,13 +3,11 @@ import '@babel/polyfill'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import { BlogList } from './BlogList'
-import { NotFound } from './NotFound'
 
 const App = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={BlogList} />
-      <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
 )
