bd008c8452390bbac88c79a94f5c5cf35205cfda
Linesh <linesh.simpcity@gmail.com>
2018-08-21 21:55:05 +0800

    [#8] ✨feature: should be able to parse front matter with only one key

--- src/react/app/BlogDetail/selectors.js
+++ src/react/app/BlogDetail/selectors.js
@@ -4,6 +4,10 @@ const getPost = (store, ownProps) => {
   return store.detail.posts[ownProps.match.params.id]
 }
 
-export const frontMatters = createSelector([getPost], () => {
-  return {}
+export const frontMatters = createSelector([getPost], (post) => {
+  const frontMatterString = post
+    .substring(post.indexOf('---') + '---'.length, post.lastIndexOf('---'))
+    .trim()
+  const [key, value] = frontMatterString.split(':').map((part) => part.trim())
+  return { [key]: value }
 })
--- src/react/app/BlogDetail/selectors.test.js
+++ src/react/app/BlogDetail/selectors.test.js
@@ -24,4 +24,31 @@ describe('frontMatters()', () => {
 
     expect(result).toEqual({})
   })
+
+  test('should return object with title when front matters contains a title', () => {
+    const store = {
+      detail: {
+        posts: {
+          1: `
+          ---
+          title: React 应用单元测试策略
+          ---
+          `,
+        },
+      },
+    }
+    const ownProps = {
+      match: {
+        params: {
+          id: 1,
+        },
+      },
+    }
+
+    const result = frontMatters(store, ownProps)
+
+    expect(result).toEqual({
+      title: 'React 应用单元测试策略',
+    })
+  })
 })
