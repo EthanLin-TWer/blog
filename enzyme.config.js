878bd1d70c6ee8a9db05f66c20025434876a6c9c
Linesh <linesh.simpcity@gmail.com>
2018-08-21 13:10:42 +0800

    [#7] 🔧chore: introduce enzyme to test component rendering logic

---
+++ enzyme.config.js
@@ -0,0 +1,7 @@
+const Enzyme = require('enzyme')
+const React16Adapter = require('enzyme-adapter-react-16')
+
+Enzyme.configure({
+  adapter: new React16Adapter(),
+  disableLifecycleMethods: true,
+})
