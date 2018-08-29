8c9e5364bb164a368d42c5f953e233c6daf72e68
Linesh <linesh.simpcity@gmail.com>
2018-08-29 10:22:51 +0800

    [#8] 🎨style: rename global styles and add margin right to checkbox

--- src/react/index.js
+++ src/react/index.js
@@ -2,6 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import { App } from './app'
-import './styles.styl'
+import './global.styl'
 
 ReactDOM.render(<App />, document.getElementById('root'))
