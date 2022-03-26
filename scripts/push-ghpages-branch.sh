3a2a57592d0950f2f7a922f2547529fc11f49772
Linesh <linesh.simpcity@gmail.com>
2022-03-26 19:07:57 +0800

    [TECH] 🔧chore: update script

--- scripts/push-ghpages-branch.sh
+++ scripts/push-ghpages-branch.sh
@@ -1,9 +1,25 @@
 #!/usr/bin/env bash
 
-npm run deploy:react
+# clean-up
+rm -rf dist
+
+# prepare dist/ and build
+git worktree add dist gh-pages
+npm run build:react
+
+# copy all necessary files to /dist for deployment to gh-pages branch
+cp -r _drafts dist/
+cp -r _posts dist/
+cp -r api dist/
+cp favicon.ico dist/favicon.ico
+cp CNAME dist/CNAME
 
 ## force push local built /dist to origin gh-pages
-git push origin `git subtree split --prefix=dist`:gh-pages --no-verify --force
+cd dist
+git add .
+git commit -m "Deploying to gh-pages from master ${$(git rev-parse HEAD):0:8}"
+git push --force --no-verify origin
+git push --force --no-verify origin gh-pages
 
 ## clean-up dist folder in local
 rm -rf dist
