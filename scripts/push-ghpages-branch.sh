85fb7bbcd076f0ea76d4565ca97f7252f5990816
Linesh <linesh.simpcity@gmail.com>
2022-03-26 18:57:36 +0800

    [TECH] 🔧chore: remove /dist from local after git push success

--- scripts/push-ghpages-branch.sh
+++ scripts/push-ghpages-branch.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 npm run deploy:react
+
+## force push local built /dist to origin gh-pages
 git push origin `git subtree split --prefix=dist`:gh-pages --no-verify --force
 
+## clean-up dist folder in local
+rm -rf dist
