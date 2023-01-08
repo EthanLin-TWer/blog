ecd4360cc212070e2c0fb8648c1647b083a31807
Ethan <linesh.simpcity@gmail.com>
2023-01-09 00:38:50 +0800

    [TECH] 🔧chore: tweak publish script

--- scripts/push-ghpages-branch.sh
+++ scripts/push-ghpages-branch.sh
@@ -4,13 +4,14 @@ headSHA=$(git rev-parse HEAD)
 
 # clean-up
 rm -rf dist
+git worktree remove -f dist
+git branch -D gh-pages
 
 # prepare dist/ and build
-git worktree add dist gh-pages
+git worktree add --track -b gh-pages dist origin/gh-pages
 npm run build:react
 
 # copy all necessary files to /dist for deployment to gh-pages branch
-cp -r _drafts dist/
 cp -r _posts dist/
 cp -r api dist/
 cp favicon.ico dist/favicon.ico
@@ -21,7 +22,9 @@ cd dist
 git add . --force
 git commit -m "🤖 Deploying to gh-pages from master ${headSHA:0:8}" --no-verify
 git push --force --no-verify origin gh-pages
+cd ..
 
 ## clean-up
 rm -rf dist
+git branch -D gh-pages
 git worktree remove -f dist
