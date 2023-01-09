c3ef36fff8860c6dd72be78484d60c6f1ddfd4fe
Ethan <linesh.simpcity@gmail.com>
2023-01-09 20:07:16 +0800

    [TECH] 🔧chore: tweak publish script

--- scripts/push-ghpages-branch.sh
+++ scripts/push-ghpages-branch.sh
@@ -8,7 +8,7 @@ git worktree remove -f dist
 git branch -D gh-pages
 
 # prepare dist/ and build
-git worktree add --track -b gh-pages dist origin/gh-pages
+git worktree add dist gh-pages
 npm run build:react
 
 # copy all necessary files to /dist for deployment to gh-pages branch
@@ -18,13 +18,15 @@ cp favicon.ico dist/favicon.ico
 cp CNAME dist/CNAME
 
 ## force push local built /dist to origin gh-pages
-cd dist
+cd dist || return
+git checkout gh-pages
 git add . --force
 git commit -m "🤖 Deploying to gh-pages from master ${headSHA:0:8}" --no-verify
 git push --force --no-verify origin gh-pages
+git checkout master
 cd ..
 
 ## clean-up
 rm -rf dist
-git branch -D gh-pages
 git worktree remove -f dist
+git branch -D gh-pages
