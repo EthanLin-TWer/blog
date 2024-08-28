c24665792ce5f7fbb0708c0f60f1b69a315ad5a7
Ethan <congyu.lin@seraitrade.com>
2024-08-28 16:08:17 +0800

    [TECH] ✨feature: support LaTex in markdown

--- src/react/components/GithubFlavoredMarkdown/index.tsx
+++ src/react/components/GithubFlavoredMarkdown/index.tsx
@@ -2,11 +2,14 @@
 import React, { FC } from 'react'
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
+import remarkMath from 'remark-math'
 import rehypeRaw from 'rehype-raw'
+import rehypeKatex from 'rehype-katex'
 
 import { CodeBlockRenderer } from './CodeBlockRenderer'
 
 import 'github-markdown-css'
+import 'katex/dist/katex.min.css'
 import './styles.styl'
 
 interface Props {
@@ -16,8 +19,8 @@ interface Props {
 export const GithubFlavoredMarkdown: FC<Props> = ({ data, className }) => (
   <ReactMarkdown
     components={{ pre: CodeBlockRenderer }}
-    remarkPlugins={[remarkGfm]}
-    rehypePlugins={[rehypeRaw]}
+    remarkPlugins={[remarkMath, remarkGfm]}
+    rehypePlugins={[rehypeRaw, rehypeKatex]}
     className={`markdown-body ${className || ''}`}
   >
     {data}
