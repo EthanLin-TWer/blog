9aec504edf74a42e5a887234f8fb58008bd19f58
Ethan <linesh.simpcity@gmail.com>
2025-04-10 22:49:42 +0800

    [TECH] 🔧chore: I think we can increase the loading time to 60s

--- src/react/utils/axios.ts
+++ src/react/utils/axios.ts
@@ -2,12 +2,12 @@ import axiosInstance from 'axios'
 
 export const axiosServer = axiosInstance.create({
   baseURL: process.env.SERVER_URL,
-  timeout: 20 * 1000,
+  timeout: 60 * 1000,
   headers: {
     'Access-Control-Allow-Origin': '*',
   },
 })
 
 export const axiosNormal = axiosInstance.create({
-  timeout: 20 * 1000,
+  timeout: 60 * 1000,
 })
